ca7dc975be8ab5ed7a3b6e101ec675453d56e45d
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-01-22 11:40:54 -0800

    adjust how we build docs so its only pdoc3

--- Makefile
+++ Makefile
@@ -4,6 +4,4 @@ test:
 	pytest -s -vvvv tests/
 
 docs:
-	mkdocs build -d built-docs
 	pdoc3 --html -o docs --force brickschema
-	pdoc3 --html -o built-docs --force brickschema
--- brickschema/__init__.py
+++ brickschema/__init__.py
@@ -1,6 +1,8 @@
 """
 Python package `brickschema` provides a set of tools, utilities and interfaces
 for working with, developing and interacting with Brick models.
+
+.. include:: ./index.md
 """
 from . import graph, inference, namespaces
 
