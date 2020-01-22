9bd35204ac676e394e591a09759a45f8f6b68539
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-01-22 12:24:53 -0800

    bumping to 0.0.2

--- brickschema/__init__.py
+++ brickschema/__init__.py
@@ -6,5 +6,5 @@ for working with, developing and interacting with Brick models.
 """
 from . import graph, inference, namespaces
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __all__ = ['graph', 'inference', 'namespaces', 'orm']
