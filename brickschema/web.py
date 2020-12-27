87bbc6c7c6c080590ec799ff0a51bfa7ab705e9b
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-12-26 22:48:05 -0700

    add Python web server impl to start

---
+++ brickschema/web.py
@@ -0,0 +1,47 @@
+"""
+Brickschema web module. This embeds a Flask webserver which provides a local web server with:
+- SPARQL interpreter + query result visualization
+- buttons to perform inference
+
+TODO:
+- implement https://www.w3.org/TR/sparql11-protocol/ on /query
+"""
+from flask import Flask, request, json, jsonify, redirect
+from rdflib.plugins.sparql.results.jsonresults import JSONResultSerializer
+import pkgutil
+import io
+
+
+class Server:
+    def __init__(self, graph):
+        self.graph = graph
+        self.app = Flask(__name__, static_url_path="/static")
+
+        self.app.route("/query", methods=["GET", "POST"])(self.query)
+        self.app.route("/", methods=["GET"])(self.home)
+
+    def query(self):
+        if request.method == "GET":
+            query = request.args.get("query")
+        elif (
+            request.method == "POST"
+            and request.content_type == "application/x-www-form-urlencoded"
+        ):
+            query = request.form.get("query")
+        elif (
+            request.method == "POST"
+            and request.content_type == "application/sparql-query"
+        ):
+            print("SPARQL", request.form.keys())
+            query = request.get_data()
+        print(query)
+        results = self.graph.query(query)
+        json_results = io.StringIO()
+        JSONResultSerializer(results).serialize(json_results)
+        return jsonify(json.loads(json_results.getvalue()))
+
+    def home(self):
+        return pkgutil.get_data(__name__, "web/index.html").decode()
+
+    def start(self):
+        self.app.run(host="localhost", port="8081")
