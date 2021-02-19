a1019c28dbeb432044e5ce8bffa7d4586480c8ec
Gabe Fierro <gtfierro@cs.berkeley.edu>
2021-02-18 20:19:43 -0800

    bump to 0.3.0a8

--- brickschema/namespaces.py
+++ brickschema/namespaces.py
@@ -21,6 +21,12 @@ RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
 SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
 SH = Namespace("http://www.w3.org/ns/shacl#")
 
+# QUDT namespaces
+QUDT = Namespace("http://qudt.org/schema/qudt/")
+QUDTQK = Namespace("http://qudt.org/vocab/quantitykind/")
+QUDTDV = Namespace("http://qudt.org/vocab/dimensionvector/")
+UNIT = Namespace("http://qudt.org/vocab/unit/")
+
 A = RDF.type
 
 
@@ -33,6 +39,9 @@ def bind_prefixes(graph, brick_version="1.2"):
     graph.bind("rdfs", RDFS)
     graph.bind("skos", SKOS)
     graph.bind("sh", SH)
+    graph.bind("qudtqk", QUDTQK)
+    graph.bind("qudt", QUDT)
+    graph.bind("unit", UNIT)
 
     if brick_version == "1.1":
         graph.bind("brick", BRICK11)
