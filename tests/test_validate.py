03dcc6e8cceef0a5fff9e6affc90ef3c1d402ef1
Gabe Fierro <gtfierro@mines.edu>
2023-12-03 11:47:58 -0700

    test_validate_bad(), test_validate_ok(), test_useExtraShapeGraph(), and test_useExtraOntGraphShapeGraph() in tests/test_validate.py to follow the style of test_useOnlyExtraShapeGraph().

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -24,7 +24,7 @@ def test_validate_bad():
     dataG = loadGraph("data/badBuilding.ttl")
     brickG = brickschema.Graph(load_brick=True)
     conforms, _, _ = dataG.validate(shape_graphs=[brickG], engine="topquadrant")
-    assert not conforms
+    assert not conforms, "expect constraint violations in badBuilding.ttl"
 
 
 def test_validate_ok():
@@ -45,9 +45,8 @@ def test_useOnlyExtraShapeGraph():
 def test_useExtraShapeGraph():
     dataG = loadGraph("data/badBuilding.ttl")
     shapeG = loadGraph("data/extraShapes.ttl")
-    g = brickschema.Graph()
-    g += dataG
-    conforms, _, _ = g.validate(shape_graphs=[shapeG])
+    brickG = brickschema.Graph(load_brick=True)
+    conforms, _, _ = dataG.validate(shape_graphs=[shapeG, brickG], engine="topquadrant")
     assert not conforms, "expect constraint violations in badBuilding.ttl"
 
 
@@ -55,16 +54,15 @@ def test_useExtraOntGraphShapeGraph():
     dataG = loadGraph("data/badBuilding.ttl")
     ontG1 = loadGraph("data/extraOntology1.ttl")
     ontG2 = loadGraph("data/extraOntology2.ttl")
-    g = brickschema.Graph(load_brick=True)
-    g += dataG
+    brickG = brickschema.Graph(load_brick=True)
 
     # Without extra shapes for the extra ontology files
     # we shouldn't see more violations
-    conforms, _, _ = g.validate(shape_graphs=[ontG1], engine="topquadrant")
+    conforms, _, _ = dataG.validate(shape_graphs=[ontG1, brickG], engine="topquadrant")
     assert not conforms, "expect constraint violations in badBuilding.ttl"
     # assert len(result.violationGraphs) == 4, "unexpected # of violations"
 
-    conforms, _, _ = g.validate(shape_graphs=[ontG1, ontG2], engine="topquadrant")
+    conforms, _, _ = dataG.validate(shape_graphs=[ontG1, ontG2, brickG], engine="topquadrant")
     assert not conforms, "expect constraint violations in badBuilding.ttl"
     # assert len(result.violationGraphs) == 4, "unexpected # of violations"
 
@@ -73,7 +71,7 @@ def test_useExtraOntGraphShapeGraph():
 
     # Add one extraShape file
     # result = v.validate(dataG, ont_graphs=[ontG1, ontG2], shacl_graphs=[shapeG1])
-    conforms, _, _ = g.validate(shape_graphs=[shapeG1, ontG1, ontG2], engine="topquadrant")
+    conforms, _, _ = dataG.validate(shape_graphs=[shapeG1, ontG1, ontG2, brickG], engine="topquadrant")
     assert not conforms, "expect constraint violations in badBuilding.ttl"
     # assert len(result.violationGraphs) == 9, "unexpected # of violations"
 
@@ -81,6 +79,6 @@ def test_useExtraOntGraphShapeGraph():
     # result = v.validate(
     #    dataG, ont_graphs=[ontG1, ontG2], shacl_graphs=[shapeG1, shapeG2]
     # )
-    conforms, _, _ = g.validate(shape_graphs=[shapeG1, shapeG2, ontG1, ontG2], engine="topquadrant")
+    conforms, _, _ = dataG.validate(shape_graphs=[shapeG1, shapeG2, ontG1, ontG2, brickG], engine="topquadrant")
     assert not conforms, "expect constraint violations in badBuilding.ttl"
     # assert len(result.violationGraphs) == 11, "unexpected # of violations"
